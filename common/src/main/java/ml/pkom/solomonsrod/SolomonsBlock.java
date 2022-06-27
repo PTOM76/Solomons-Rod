2ea709711f0008fce6ace9c630db8988303c59a1
PTOM76 <ptms76@outlook.jp>
2022-06-28 00:02:05 +0900

    backport to 1.18.2

--- common/src/main/java/ml/pkom/solomonsrod/SolomonsBlock.java
+++ common/src/main/java/ml/pkom/solomonsrod/SolomonsBlock.java
@@ -11,11 +11,12 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class SolomonsBlock extends Block {
 

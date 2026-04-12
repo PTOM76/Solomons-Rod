6c35471c698691a2c818f00d17293d66e42b2a7a
PTOM76 <ptms76@outlook.jp>
2026-04-12 23:48:09 +0900

    refactor HighlightRenderer to improve position calculations and remove unused imports

--- common/src/main/java/net/pitan76/solomonsrod/renderer/HighlightRenderer.java
+++ common/src/main/java/net/pitan76/solomonsrod/renderer/HighlightRenderer.java
@@ -7,7 +7,6 @@ import net.pitan76.mcpitanlib.api.util.VoxelShapeUtil;
 import net.pitan76.mcpitanlib.api.util.client.ClientUtil;
 import net.pitan76.mcpitanlib.midohra.client.render.CameraWrapper;
 import net.pitan76.mcpitanlib.midohra.item.ItemStack;
-import net.pitan76.mcpitanlib.midohra.util.math.BlockPos;
 import net.pitan76.mcpitanlib.midohra.util.math.Vector3d;
 import net.pitan76.solomonsrod.SolomonsWand;
 
@@ -27,15 +26,13 @@ public class HighlightRenderer implements WorldRenderContextListener {
         if (!(stack.getItem().instanceOf(SolomonsWand.class))) return;
 
         CameraWrapper camera = e.getCameraWrapper();
-        BlockPos blockPos = SolomonsWand.getPlacingPos(player);
-
+        Vector3d blockPos = SolomonsWand.getPlacingPos(player).toVector3d();
         Vector3d camPos = camera.getCameraPos();
-        double x = blockPos.getX() - camPos.x;
-        double y = blockPos.getY() - camPos.y;
-        double z = blockPos.getZ() - camPos.z;
+
+        Vector3d pos = blockPos.sub(camPos);
 
         e.push();
-        e.translate(x, y, z);
+        e.translate(pos.x, pos.y, pos.z);
         e.drawBox(VoxelShapeUtil.getBoundingBox(VoxelShapeUtil.fullCube()), 1f, 1f, 1f, 1f);
         e.pop();
     }
